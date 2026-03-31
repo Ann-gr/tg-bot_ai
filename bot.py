4ee62c37127726800f0edc382d1f2d8fa692363d
Ann <meannok@mail.ru>
2026-03-31 13:39:04 +0300

    fake web server

--- bot.py
+++ bot.py
@@ -1,22 +1,25 @@
-from telegram.ext import (
-    ApplicationBuilder,
-    CommandHandler,
-    MessageHandler,
-    filters
-)
+import threading
+from flask import Flask
+
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
 
 from config import TOKEN
-
 from handlers.commands import start, help_command
 from handlers.messages import handle_message
 
+app_flask = Flask(__name__)
 
-def main():
+
+@app_flask.route("/")
+def home():
+    return "Bot is running!"
+
+
+def run_bot():
     app = ApplicationBuilder().token(TOKEN).build()
 
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CommandHandler("help", help_command))
-
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
 
     print("Бот запущен...")
@@ -24,4 +27,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    # запускаем бота в отдельном потоке
+    threading.Thread(target=run_bot).start()
+
+    # запускаем веб-сервер (Render доволен)
+    app_flask.run(host="0.0.0.0", port=10000)
