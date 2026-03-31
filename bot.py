37ca38e95b4d157e4ca09cf81685ad03585388d1
Ann <meannok@mail.ru>
2026-03-31 15:50:08 +0300

    tests

--- bot.py
+++ bot.py
@@ -1,46 +1,36 @@
 from flask import Flask, request
-from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters
+from telegram import Update, Bot
 from config import TOKEN
-import os
 
 app = Flask(__name__)
+bot = Bot(TOKEN)
 
-# Telegram app
-tg_app = ApplicationBuilder().token(TOKEN).build()
 
-# handlers
-async def start(update, context):
-    await update.message.reply_text("Бот работает 🚀")
-
-async def echo(update, context):
-    await update.message.reply_text(update.message.text)
-
-tg_app.add_handler(CommandHandler("start", start))
-tg_app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
-
-# webhook endpoint
-@app.route(f"/webhook/{TOKEN}", methods=["POST"])
-def webhook():
-    print("🔥 WEBHOOK HIT")
-    data = request.get_json(force=True)
-    print("DATA:", data)
-
-    update = Update.de_json(data, tg_app.bot)
-
-    # отправляем update в telegram app
-    tg_app.create_task(tg_app.process_update(update))
-
-    return "ok"
-
-# health check для Render
 @app.route("/", methods=["GET"])
 def home():
     return "Bot is running"
 
-if __name__ == "__main__":
-    print("Starting bot...")
-    print("TOKEN exists:", TOKEN is not None)
 
-    PORT = int(os.getenv("PORT", 10000))
-    app.run(host="0.0.0.0", port=PORT)
+@app.route(f"/webhook/{TOKEN}", methods=["POST"])
+def webhook():
+    data = request.get_json(force=True)
+
+    update = Update.de_json(data, bot)
+
+    print("🔥 UPDATE:", data)
+
+    if update.message:
+        text = update.message.text
+
+        if text == "/start":
+            bot.send_message(
+                chat_id=update.message.chat.id,
+                text="Бот работает 🚀"
+            )
+        else:
+            bot.send_message(
+                chat_id=update.message.chat.id,
+                text=f"Ты написал: {text}"
+            )
+
+    return "ok"
