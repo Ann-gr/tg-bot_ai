f33630ec96cc987e78af0e18fe11a2f03968f7a3
Ann <meannok@mail.ru>
2026-03-31 15:30:48 +0300

    fix set_webhook.py

--- set_webhook.py
+++ set_webhook.py
@@ -1,12 +1,15 @@
 import asyncio
-from config import TOKEN
 from telegram import Bot
+from config import TOKEN
 
-URL = "https://ai-tg-bot-lf1m.onrender.com/webhook/" + TOKEN
+URL = f"https://ai-tg-bot-lf1m.onrender.com/webhook/{TOKEN}"
 
 async def main():
     bot = Bot(TOKEN)
+
+    await bot.delete_webhook()
     await bot.set_webhook(url=URL)
-    print("Webhook set!")
+
+    print("Webhook set:", URL)
 
 asyncio.run(main())
