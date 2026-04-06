0b897b8ef0ebda36c54dbf419a3151aa036e6b66
Ann <meannok@mail.ru>
2026-04-06 16:53:13 +0300

    db

--- state/db/init_db.py
+++ state/db/init_db.py
@@ -4,10 +4,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "state.db")
 
+
 def init_db():
     print("🔥 INIT DB CALLED")
     print("DB PATH:", DB_PATH)
-    
+
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
@@ -19,4 +20,9 @@ def init_db():
     """)
 
     conn.commit()
+    print("✅ TABLE CREATED OR EXISTS")
+
+    cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
+    print("📦 TABLES IN DB:", cursor.fetchall())
+
     conn.close()
