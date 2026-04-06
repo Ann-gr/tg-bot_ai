6b5190d5e85cf0e04c79e1c5253684f2b6685aff
Ann <meannok@mail.ru>
2026-04-06 17:06:44 +0300

    db

--- state/db/init_db.py
+++ state/db/init_db.py
@@ -4,25 +4,40 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DB_PATH = os.path.join(BASE_DIR, "state.db")
 
-
 def init_db():
     print("🔥 INIT DB CALLED")
-    print("DB PATH:", DB_PATH)
 
     conn = sqlite3.connect(DB_PATH)
     cursor = conn.cursor()
 
+    # пересоздаём таблицу
+    cursor.execute("DROP TABLE IF EXISTS user_state")
+
     cursor.execute("""
-    CREATE TABLE IF NOT EXISTS user_state (
+    CREATE TABLE user_state (
         user_id TEXT PRIMARY KEY,
-        state TEXT
+        mode TEXT,
+        params TEXT,
+        last_text TEXT,
+        last_result TEXT
+    )
+    """)
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS history (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        user_id TEXT,
+        role TEXT,
+        content TEXT,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     )
     """)
 
     conn.commit()
-    print("✅ TABLE CREATED OR EXISTS")
+
+    print("✅ TABLES CREATED")
 
     cursor.execute("SELECT name FROM sqlite_master WHERE type='table'")
-    print("📦 TABLES IN DB:", cursor.fetchall())
+    print("📦 TABLES:", cursor.fetchall())
 
     conn.close()
